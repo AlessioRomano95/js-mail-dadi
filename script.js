e0bb5981389fbc1b736f839916b2701140687616
AlessioRomano95 <romanalex400@gmail.com>
2024-02-25 06:19:39 +0100

    [ADD] email e readme

--- script.js
+++ script.js
@@ -25,3 +25,46 @@ button.addEventListener('click', function(){
 
     victory.innerText = message + ' risultato utente: ' + resultUser + ' risultato computer: ' + resultCpu
 })
+
+
+// es email
+const email = ['armando@libero.it',
+  'ALexciao@icloud.com',
+  'ironman@gmail.com',
+  'obione@gmail.com'
+  'spock@gmail.com']
+
+const emailUser = document.getElementById('email')
+const emailButton = document.getElementById('mail-button')
+const text = document.getElementById('text')
+
+emailButton.addEventListener('click', function(){
+
+const emailDichiarata = emailUser.value.trim()
+console.log(emailDichiarata)
+
+if (!emailDichiarata){
+    alert('Email non valida')
+    return
+}
+
+let userAutorizzato = false
+
+for(let i = 0; i < email.length && !userAutorizzato; i++){
+    console.log(email[i])
+    let messageForUser
+
+if (emailDichiarata === email[i]){
+    userAutorizzato = true
+    console.log('Benvenuto ', emailDichiarata)
+    console.log(userAutorizzato)
+    messageForUser = 'Benvenuto ' + emailDichiarata
+} else if(!userAutorizzato){
+    console.log('Non puoi entrare ', emailDichiarata)
+    console.log(userAutorizzato)
+    messageForUser = 'Non sei autorizzato '+ emailDichiarata
+}
+
+text.innerText = messageForUser
+}
+})
